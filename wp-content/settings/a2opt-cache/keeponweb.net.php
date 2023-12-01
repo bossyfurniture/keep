b210b059dee747dc0b3cb074b7f66fa63d50594b
unknown <Abbas Furniturewalla>
2023-12-01 11:42:06 -0500

    upload keep files

---
+++ wp-content/settings/a2opt-cache/keeponweb.net.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * A2 Optimized Cache settings for keeponweb.net
+ *
+ * @since      1.5.0
+ * @change     1.5.0
+ *
+ * @generated  25.09.2023 17:59:40
+ */
+
+return array (
+  'version' => '3.0',
+  'permalink_structure' => 'has_trailing_slash',
+  'cache_expires' => 0,
+  'cache_expiry_time' => 0,
+  'clear_site_cache_on_saved_post' => 1,
+  'clear_site_cache_on_saved_comment' => 1,
+  'clear_site_cache_on_changed_plugin' => 1,
+  'compress_cache' => 1,
+  'convert_image_urls_to_webp' => 0,
+  'excluded_post_ids' => '',
+  'excluded_page_paths' => '',
+  'excluded_query_strings' => '',
+  'excluded_cookies' => '',
+  'minify_html' => 1,
+  'minify_inline_css_js' => 0,
+);
